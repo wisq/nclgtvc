970a9ee795a8df73f98a4de843490f3effa04c2d
Adrian Irving-Beer <wisq@wisq.net>
2018-08-24 22:02:25 -0400

    New project from "mix new nclgtvc".

---
+++ lib/nclgtvc.ex
@@ -0,0 +1,18 @@
+defmodule Nclgtvc do
+  @moduledoc """
+  Documentation for Nclgtvc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nclgtvc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nclgtvc_test.exs
@@ -0,0 +1,8 @@
+defmodule NclgtvcTest do
+  use ExUnit.Case
+  doctest Nclgtvc
+
+  test "greets the world" do
+    assert Nclgtvc.hello() == :world
+  end
+end
